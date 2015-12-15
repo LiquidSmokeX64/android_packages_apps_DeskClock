afc5e64b85078c8b6d23b1f6447d51b82110baad
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2015-12-15 01:22:15 -0800

    Odexing

--- Android.mk
+++ Android.mk
@@ -35,4 +35,7 @@ LOCAL_AAPT_FLAGS += --extra-packages com.android.datetimepicker
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+# Enable dexpreopt to speed boot time
+WITH_DEXPREOPT := true
+
 include $(BUILD_PACKAGE)
